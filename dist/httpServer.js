d856613891b9b30560a0a68ad9fcd8f0ca3d17d2
unknown <ll@gmail.com>
2024-07-31 14:15:47 +0700

    fix:httpServer response

--- dist/httpServer.js
+++ dist/httpServer.js
@@ -9,7 +9,8 @@ http_1.default.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     let q = url_1.default.parse(req.url, true).query;
     let txt = q.year + " " + q.month;
-    res.end(txt);
+    const { firstName, lastName, month, date, year } = q;
+    res.end(`<div><div>${firstName} ${lastName}</div><div>Login Date:${month} ${date},${year}</div></div>`);
 }).listen(3100, () => {
     console.log(`Server is running of port ${3100}`);
 });
--- src/httpServer.ts
+++ src/httpServer.ts
@@ -6,7 +6,8 @@ http.createServer((req:any,res)=>{
 
     let q = url.parse(req.url,true).query
     let txt = q.year + " " + q.month
-    res.end(txt)
+    const {firstName,lastName,month,date,year} = q
+    res.end(`<div><div>${firstName} ${lastName}</div><div>Login Date:${month} ${date},${year}</div></div>`)
 }).listen(3100,()=>{
     console.log(`Server is running of port ${3100}`)
 })
